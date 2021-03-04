55bf225900c418b65336cc6abacf5a5b07c57ad1
Arriv9l <imaginistx12@gmail.com>
2021-03-04 18:05:39 +0800

    test5.8

---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core')
+const fs = require('fs')
+
+function getJSON(url) {
+    return new Promise((resolve, reject) => {
+        request.get(url, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                resolve(body);
+            }
+        });
+    });
+}
+
+(async () => {
+    const repository = core.getInput('repository');
+    const before = core.getInput('before');
+    const after = core.getInput('after');
+    console.log(`repository: ${repository}`);
+    const url = `https://api.github.com/repos/${repository}/compare/${before}...${after}`
+    const json = JSON.parse(await getJSON(url));
+    const files = json['files'];
+    console.log(`files: ${files}`);
+
+    let paths = files
+        .map(x => x['filename'].match(/part\d\/.+?(?=\/)/))
+        .filter(Boolean);
+    console.log(`paths0: ${paths}`);
+    paths = [...new Set(paths)];
+    console.log(`paths1: ${paths}`);
+})();
